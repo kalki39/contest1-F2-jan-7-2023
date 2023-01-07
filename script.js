c9d22a5bb016c610901c29d733820d683b7a1d11
kalki39 <kalkiram39@gmail.com>
2023-01-07 20:30:27 +0530

    first

--- script.js
+++ script.js
@@ -7,21 +7,65 @@ let arr = [
 ];
 
 function PrintDeveloperswithMap() {
-  //Write your code here
+  arr.map((element) => {
+    if (element.profession == "developer") {
+      console.log(element);
+    }
+  })
+
+  // another way of writing-------------------------------//
+
+
+  // arr.map(printingThroghMap); // Each element is stored in "printingThroghMap" variable
+
+  // function printingThroghMap(arrayItem) {
+  //   if (arrayItem.profession === "developer") {
+  //     console.log(arrayItem);
+  //   }
+  // }
 }
 
 function PrintDeveloperbyForEach() {
-  //Write your code here
+  arr.forEach((element) => {
+    if (element.profession == "developer") {
+      console.log(element);
+    }
+  })
 }
 
 function addData() {
-  //Write your code here
+  let flag=false;                   //condition check if already 4th element with "id:4" is present in array it not allow
+  arr.forEach((element) => {          //to add element again a same element ,only display the element in array
+    if (element.profession == "intern") {
+      flag=true;
+    }
+  })
+
+  if(flag==false){
+  arr.push({id:4,name:"susan",age:"20",profession:"intern"})
+  
+  }
+  console.log(arr)
 }
 
 function removeAdmin() {
-  //Write your code here
+  arr.filter((element) => {
+    if (element.profession != "admin") {
+      console.log(element);
+    }
+  })
 }
 
 function concatenateArray() {
-  //Write your code here
+  let arr2 = [
+    { id: 5, name: "jacob", age: "28", profession: "team leader" },
+    { id: 6, name: "jenni", age: "30", profession: "ux designer" },
+    { id: 9, name: "karthik", age: "49", profession: "QA tester" },
+  ];
+
+  let result=arr.concat(arr2)
+  console.log(result)
+  // for(num of arr){
+  //   console.log(num)
+  //   }
 }
